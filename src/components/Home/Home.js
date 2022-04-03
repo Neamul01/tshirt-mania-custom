4c540f064cb7991e3c0ad50db7e3d47e901772a8
Neamul01 <neamulhaque2002@gmail.com>
2022-04-03 13:00:51 +0600

    code starting today so save all past data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import useTshirt from '../../hooks/useTshirt';
 import Cart from '../Cart/Cart';
 
 const Home = () => {
+    const [tshirt, setTshirt] = useTshirt()
     return (
         <div className='home-container'>
             <div className="tshirt-container">
-
+                <h2>{tshirt.length}</h2>
             </div>
             <div className="cart-container">
                 <Cart></Cart>
---
+++ src/hooks/useTshirt.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+
+const useTshirt = () => {
+    const [tshirt, setTshirt] = useState([]);
+    useEffect(() => {
+        fetch('tshirts.json').then(res => res.json()).then(data => setTshirt(data))
+    }, [])
+    return [tshirt, setTshirt]
+}
+export default useTshirt;
